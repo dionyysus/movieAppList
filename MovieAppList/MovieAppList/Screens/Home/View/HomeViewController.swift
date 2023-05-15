221a333d99897be90be316847cb89259ec350398
dionyysus <gizemcsk1@gmail.com>
2023-05-15 22:19:51 +0300

    search text field completed

--- MovieAppList/MovieAppList/Screens/Home/View/HomeViewController.swift
+++ MovieAppList/MovieAppList/Screens/Home/View/HomeViewController.swift
@@ -9,29 +9,28 @@ import UIKit
 
 // movie api: https://api.themoviedb.org/3/discover/movie?api_key=d0cb5f9ae1c996d1bd22dc17e287debd
 
-class HomeViewController: UIViewController, UITextFieldDelegate{
+class HomeViewController: UIViewController, UITextFieldDelegate {
 
-    private var movies: [Movie]?
+    private var movies = [Movie]()
     private var genres: [Genre]?
     
     //var baseApiURL = "https://api.themoviedb.org/3/"
     var apiURL = "https://api.themoviedb.org/3/discover/movie?api_key=d0cb5f9ae1c996d1bd22dc17e287debd"
     var genreApiURL = "https://api.themoviedb.org/3/genre/movie/list?api_key=d0cb5f9ae1c996d1bd22dc17e287debd"
     let imgUrl = "http://image.tmdb.org/t/p/w500"
-   
-
+    
+    var searching = false
     var filteredMovies = [Movie]() // Arama sonucu listesi
     
+    @IBOutlet weak var searchBar: UISearchBar!
     @IBOutlet weak var moviesCollectionView: UICollectionView!
     @IBOutlet var favoriteMoviesCollectionView: UICollectionView!
-    @IBOutlet var searchTextField: UITextField!
-
     @IBOutlet weak var categoryCollectionView: UICollectionView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         title = "Home"
-
+        
         let nib = UINib(nibName: MoviesCollectionViewCell.identifier, bundle: nil)
         moviesCollectionView.register(nib, forCellWithReuseIdentifier: MoviesCollectionViewCell.identifier)
         
@@ -39,17 +38,10 @@ class HomeViewController: UIViewController, UITextFieldDelegate{
         moviesCollectionView.delegate = self
         categoryCollectionView.dataSource = self
         categoryCollectionView.delegate = self
-        searchTextField.delegate = self
-        
-
-//        let layout = UICollectionViewFlowLayout()
-//        layout.scrollDirection = .horizontal
-//        layout.minimumLineSpacing = 5
-//        layout.minimumInteritemSpacing = 5
-//        categoryCollectionView.setCollectionViewLayout(layout, animated: true)
-        
+        //searchTextField.delegate = self
+  
         APIManager.shared.fetchMovies(url: apiURL) { (data: MovieResponse?) in
-            self.movies = data?.results
+            self.movies = data!.results!
             DispatchQueue.main.async {
                 self.moviesCollectionView.reloadData()
                 self.categoryCollectionView.reloadData()
@@ -62,19 +54,32 @@ class HomeViewController: UIViewController, UITextFieldDelegate{
                 self.moviesCollectionView.reloadData()
                 self.categoryCollectionView.reloadData()
             }
-            //print("ggwp data \(data)")
         }
+       
     }
+    
+    @IBAction func searchHandler(_ sender: UITextField) {
+        if let searchText = sender.text{
+            filteredMovies = searchText.isEmpty ? movies : movies.filter {$0.title!.lowercased().contains(searchText.lowercased())}
+            searching = true
+        }
+        if sender.text!.isEmpty {
+            searching = false
+        }
+        moviesCollectionView.reloadData()
+    }
+    
 }
-
 extension HomeViewController: UICollectionViewDataSource{
-    
-    
     
     func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
         
         if collectionView == moviesCollectionView {
-            return movies?.count ?? 00
+            if searching {
+                return filteredMovies.count
+            }else{
+                return movies.count
+            }
         } else if collectionView == categoryCollectionView {
             return genres?.count ?? 00
         } else {
@@ -94,18 +99,31 @@ extension HomeViewController: UICollectionViewDataSource{
             guard let cell = moviesCollectionView.dequeueReusableCell(withReuseIdentifier: "MoviesCollectionViewCell", for: indexPath) as? MoviesCollectionViewCell else {
                 return UICollectionViewCell()
             }
-            cell.movieNameLabel.text = movies?[indexPath.row].title
-            cell.movieVoteLabel.text = String(movies?[indexPath.row].voteAverage ?? 0.0)
+            if searching{
+                cell.movieNameLabel.text = filteredMovies[indexPath.row].title
+                cell.movieVoteLabel.text = String(filteredMovies[indexPath.row].voteAverage ?? 0.0)
 
-            let imgPosterPath = movies?[indexPath.row].posterPath ?? ""
-            let imgFullPath = URL(string: "\(imgUrl + imgPosterPath)")
+                let imgPosterPath = filteredMovies[indexPath.row].posterPath ?? ""
+                let imgFullPath = URL(string: "\(imgUrl + imgPosterPath)")
 
-            cell.movieImageView.loadImg(url: imgFullPath!)
-          
-            movies?[indexPath.row].genreIDS?.forEach{print("ggwp \($0)")}
-          
+                cell.movieImageView.loadImg(url: imgFullPath!)
+              
+                filteredMovies[indexPath.row].genreIDS?.forEach{print("ggwp \($0)")}
+              
+            }else{
+                cell.movieNameLabel.text = movies[indexPath.row].title
+                cell.movieVoteLabel.text = String(movies[indexPath.row].voteAverage ?? 0.0)
+
+                let imgPosterPath = movies[indexPath.row].posterPath ?? ""
+                let imgFullPath = URL(string: "\(imgUrl + imgPosterPath)")
+
+                cell.movieImageView.loadImg(url: imgFullPath!)
+              
+                movies[indexPath.row].genreIDS?.forEach{print("ggwp \($0)")}
+
+            }
+           
             return cell
-            
         }
 
         else if collectionView == categoryCollectionView {
@@ -118,95 +136,25 @@ extension HomeViewController: UICollectionViewDataSource{
         
         return UICollectionViewCell()
         
-        
-//        if searchTextField.text!.isEmpty {
-//            
-//            if collectionView == moviesCollectionView {
-//                guard let cell = moviesCollectionView.dequeueReusableCell(withReuseIdentifier: "MoviesCollectionViewCell", for: indexPath) as? MoviesCollectionViewCell else {
-//                    return UICollectionViewCell()
-//                }
-//                cell.movieNameLabel.text = movies?[indexPath.row].title
-//                cell.movieVoteLabel.text = String(movies?[indexPath.row].voteAverage ?? 0.0)
-//
-//                let imgPosterPath = movies?[indexPath.row].posterPath ?? ""
-//                let imgFullPath = URL(string: "\(imgUrl + imgPosterPath)")
-//
-//                cell.movieImageView.loadImg(url: imgFullPath!)
-//              
-//                movies?[indexPath.row].genreIDS?.forEach{print("ggwp \($0)")}
-//              
-//                return cell
-//                
-//            }
-//
-//            else if collectionView == categoryCollectionView {
-//                        guard let catecoryCell = categoryCollectionView.dequeueReusableCell(withReuseIdentifier: "CategoriesCollectionViewCell", for: indexPath) as? CategoriesCollectionViewCell else {
-//                            return UICollectionViewCell()
-//                        }
-//                catecoryCell.categoryNameLabel.text = genres?[indexPath.row].name
-//                        return catecoryCell
-//                    }
-//            
-//            return UICollectionViewCell()
-//        } else {
-//            if collectionView == moviesCollectionView {
-//                guard let cell = moviesCollectionView.dequeueReusableCell(withReuseIdentifier: "MoviesCollectionViewCell", for: indexPath) as? MoviesCollectionViewCell else {
-//                    return UICollectionViewCell()
-//                }
-//                cell.movieNameLabel.text = filteredMovies[indexPath.row].title
-//                cell.movieVoteLabel.text = String(filteredMovies[indexPath.row].voteAverage ?? 0.0)
-//                
-//                let imgPosterPath = filteredMovies[indexPath.row].posterPath ?? ""
-//                let imgFullPath = URL(string: "\(imgUrl + imgPosterPath)")
-//                
-//                cell.movieImageView.loadImg(url: imgFullPath!)
-//                
-//                cell.movieCategoryNameLabel.text = "\(filteredMovies[indexPath.row].genreIDS)"
-//  
-//                return cell
-//                
-//                    }
-//            
-//            else if collectionView == categoryCollectionView {
-//                        guard let catecoryCell = categoryCollectionView.dequeueReusableCell(withReuseIdentifier: "CategoriesCollectionViewCell", for: indexPath) as? CategoriesCollectionViewCell else {
-//                            return UICollectionViewCell()
-//                        }
-//                
-//                catecoryCell.categoryNameLabel.text = "\(genres?[indexPath.row].name)"
-//                
-//                        return catecoryCell
-//                    }
-//            return UICollectionViewCell()
-//        }
-//        
-        
+
     }
 }
+
 extension HomeViewController: UICollectionViewDelegate{
-    
+
     func textField(_ textField: UITextField, shouldChangeCharactersIn range: NSRange, replacementString string: String) -> Bool {
-           let searchString = (textField.text! as NSString).replacingCharacters(in: range, with: string)
-        
+
+        let searchString = (textField.text! as NSString).replacingCharacters(in: range, with: string)
+
            if searchString.isEmpty {
-               filteredMovies = movies!
+               filteredMovies = movies
            } else {
-               filteredMovies = (movies?.filter { movie in
+               filteredMovies = (movies.filter { movie in
                    return (movie.title?.lowercased().contains(searchString.lowercased()))!
-               })!
+                   searching = true
+               })
            }
            moviesCollectionView.reloadData()
            return true
        }
 }
-
-//extension HomeViewController: UICollectionViewDelegateFlowLayout{
-//    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, insetForSectionAt section: Int) -> UIEdgeInsets {
-//        return UIEdgeInsets(top: 1.0, left: 1.0, bottom: 1.0, right: 1.0)
-//    }
-//
-//    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
-//        let gridLayout = collectionViewLayout as! UICollectionViewFlowLayout
-//        let widthPerItem = collectionView.frame.width / 2 - gridLayout.minimumInteritemSpacing
-//        return CGSize(width:widthPerItem, height:100)
-//    }
-//}
