d40cc3c4d8745a42a495b10305419f26f0765743
dionyysus <gizemcsk1@gmail.com>
2023-07-07 00:04:39 +0300

    search storyboard references done

--- MovieAppList/MovieAppList/Screens/Search/SearchViewController.swift
+++ MovieAppList/MovieAppList/Screens/Search/SearchViewController.swift
@@ -1,8 +1,29 @@
 //
-//  Search.swift
+//  SearchViewController.swift
 //  MovieAppList
 //
-//  Created by Gizem Coşkun on 6.07.2023.
+//  Created by Gizem Coşkun on 7.07.2023.
 //
 
-import Foundation
+import UIKit
+
+class SearchViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
