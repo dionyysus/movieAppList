788e65c14ba4ac332957bfd612dfcb98f6da9444
dionyysus <gizemcsk1@gmail.com>
2023-07-16 14:03:38 +0300

    selected categories more than one

--- MovieAppList/MovieAppList/Screens/Home/View/HomeViewController.swift
+++ MovieAppList/MovieAppList/Screens/Home/View/HomeViewController.swift
@@ -10,6 +10,7 @@ class HomeViewController: UIViewController, UITextFieldDelegate {
     var selectedCategory = false
     var isSelectedCell = false
     var deSelectCell = false
+    var selectedCategories: [Int] = []
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -59,13 +60,13 @@ extension HomeViewController: UICollectionViewDataSource {
     func collectionView(_ collectionView: UICollectionView,
                         numberOfItemsInSection section: Int) -> Int {
         if collectionView == moviesCollectionView {
-            if selectedCategory {
-                return viewModel?.categoryMovies?.count ?? 0
-            } else {
+            if selectedCategories.isEmpty {
                 return viewModel?.movies?.count ?? 0
+            } else {
+                return viewModel?.movies?.filter { movie in
+                    movie.genreIDS.contains(where: { selectedCategories.contains($0 ?? 0) })
+                }.count ?? 0
             }
-            
-            //  MARK: CATEGORY COLLECTION DATA SOURCE CELL COUNT
         } else if collectionView == categoryCollectionView {
             return viewModel?.genres?.count ?? 0
         } else {
@@ -74,47 +75,55 @@ extension HomeViewController: UICollectionViewDataSource {
     }
     
     //  MARK: - MOVIE COLLECTION DATA SOURCE CELL CONTENT
-    func collectionView(_ collectionView: UICollectionView,
-                        cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
-        
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
         if collectionView == moviesCollectionView {
-            guard let cell = moviesCollectionView.dequeueReusableCell(withReuseIdentifier: "MoviesCollectionViewCell", for: indexPath) as? MoviesCollectionViewCell
-            else {
+            guard let cell = moviesCollectionView.dequeueReusableCell(withReuseIdentifier: "MoviesCollectionViewCell", for: indexPath) as? MoviesCollectionViewCell else {
                 return UICollectionViewCell()
             }
             
-            let funkNasty: [Movie]? = {
-                if selectedCategory {
-                    return viewModel?.categoryMovies
-                } else {
+            let filteredMovies: [Movie]? = {
+                if selectedCategories.isEmpty {
                     return viewModel?.movies
+                } else {
+                    return viewModel?.movies?.filter { movie in
+                        movie.genreIDS.contains(where: { selectedCategories.contains($0 ?? 0) })
+                    }
                 }
             }()
             
-            cell.movieNameLabel.text = funkNasty?[indexPath.row].title
-            cell.movieVoteLabel.text = String(funkNasty?[indexPath.row].voteAverage ?? 0.0)
+            let movie = filteredMovies?[indexPath.item]
             
-            let imgPosterPath = funkNasty?[indexPath.row].posterPath ?? ""
-            let imgFullPath = URL(string: "\(APIManager.shared.imgUrl + imgPosterPath)")
+            cell.movieNameLabel.text = movie?.title
+            cell.movieVoteLabel.text = String(movie?.voteAverage ?? 0.0)
             
-            cell.movieImageView.loadImg(url: imgFullPath!)
+            if let posterPath = movie?.posterPath,
+               let imgUrl = URL(string: "\(APIManager.shared.imgUrl + posterPath)") {
+                cell.movieImageView.loadImg(url: imgUrl)
+            } else {
+                // Poster yüklenemediğinde varsayılan görsel kullanılabilir
+                cell.movieImageView.image = UIImage(named: "default_poster")
+            }
             
-            let movieGenres = funkNasty?[indexPath.row].genreIDS
-            let genreName = viewModel?.genres?.filter { genre in
-                movieGenres!.contains(genre.id ?? 0) // ! ekledim movieGenres yanına (realm için)
-            }.map { $0.name ?? "" }.joined(separator: ",")
+            let genreIDs = movie?.genreIDS
+            let genreNames = genreIDs?.compactMap { genreID in
+                self.viewModel?.genres?.first { genre in genre.id == genreID }?.name
+            }.joined(separator: ",")
             
-            cell.movieCategoryNameLabel.text = genreName
-//            funkNasty?[indexPath.row].genreIDS.forEach{print("Genre ID: \($0)")}
+            cell.movieCategoryNameLabel.text = genreNames
+            
             return cell
-            
-            //  MARK: - CATEGORY COLLECTION DATA SOURCE CELL CONTENT
-            // fonksiyon oluştur
         } else if collectionView == categoryCollectionView {
             guard let categoryCell = categoryCollectionView.dequeueReusableCell(withReuseIdentifier: "CategoriesCollectionViewCell", for: indexPath) as? CategoriesCollectionViewCell else {
                 return UICollectionViewCell()
             }
             categoryCell.categoryNameLabel.text = viewModel?.genres?[indexPath.row].name
+            if selectedCategories.contains(viewModel?.genres?[indexPath.row].id ?? 0) {
+                categoryCell.layer.borderColor =  UIColor.darkGray.cgColor
+                categoryCell.layer.borderWidth = 3.0
+            } else {
+                categoryCell.layer.borderColor =  UIColor.white.cgColor
+            }
             return categoryCell
         }
         
@@ -126,44 +135,51 @@ extension HomeViewController: UICollectionViewDelegate {
     // MARK: - MOVIE COLLECTION DELEGATE
     func collectionView(_ collectionView: UICollectionView,
                         didSelectItemAt indexPath: IndexPath) {
-        
         if collectionView == moviesCollectionView {
-            let storyBoard = UIStoryboard(name: "Detail",
-                                          bundle: nil)
+            let storyBoard = UIStoryboard(name: "Detail", bundle: nil)
             let gotoDetailController = storyBoard.instantiateViewController(withIdentifier: "DetailViewController") as! DetailViewController
             
-            let movie = viewModel?.categoryMovies?[indexPath.row]  //save index-collections
-            
-            let movieGenres = viewModel?.categoryMovies?[indexPath.row].genreIDS
-            let genreName = viewModel?.genres?.filter { genre in
-                movieGenres!.contains(genre.id ?? 0) // ! ekledim movieGenres yanına (realm için)
-            }.map { $0.name ?? "" }.joined(separator: ",") ?? ""
-            gotoDetailController.movieId = indexPath.row
-            if let movie {
-                gotoDetailController.prepare(movie: movie, genreName: genreName)
-                navigationController?.pushViewController(gotoDetailController, animated: true)
-            }
-            return
-            
-            //   MARK: CATEGORY COLLECTION DELEGATE
-        } else if collectionView == categoryCollectionView {
-            if let cell = categoryCollectionView.cellForItem(at: indexPath) as? CategoriesCollectionViewCell {
-                if !isSelectedCell {
-                    cell.layer.borderColor =  UIColor.darkGray.cgColor
-                    cell.layer.borderWidth = 3.0
-                    cellClicked(indexPath: indexPath)
-                    isSelectedCell = true
+            let filteredMovies: [Movie]? = {
+                if selectedCategories.isEmpty {
+                    return viewModel?.movies
                 } else {
-                    if !deSelectCell {
-                        cell.layer.borderColor =  UIColor.white.cgColor
-                        isSelectedCell = false
-//                        viewModel?.fetchMovies { [weak self] in
-//                            self?.moviesCollectionView.reloadData()
-//                            self?.categoryCollectionView.reloadData()
-//                        }
+                    return viewModel?.movies?.filter { movie in
+                        movie.genreIDS.contains(where: { selectedCategories.contains($0 ?? 0) })
                     }
                 }
+            }()
+            
+            let movie = filteredMovies?[indexPath.row]
+            
+            let movieGenres = movie?.genreIDS
+            let genreName = viewModel?.genres?
+                .filter { genre in movieGenres?.contains(genre.id ?? 0) ?? false }
+                .compactMap { $0.name }
+                .joined(separator: ",") ?? ""
+            
+            if let selectedMovie = movie {
+                gotoDetailController.prepare(movie: selectedMovie, genreName: genreName)
+                navigationController?.pushViewController(gotoDetailController, animated: true)
             }
+            
+            return
+            //   MARK: CATEGORY COLLECTION DELEGATE
+        }
+        else if collectionView == categoryCollectionView {
+            let selectedCategoryId = viewModel?.genres?[indexPath.row].id ?? 0
+            if selectedCategories.contains(selectedCategoryId) {
+                
+                // Kategori zaten seçili, seçimini kaldır
+                if let index = selectedCategories.firstIndex(of: selectedCategoryId) {
+                    selectedCategories.remove(at: index)
+                }
+            } else {
+                // Kategori seçilmedi, seçimini ekle
+                selectedCategories.append(selectedCategoryId)
+            }
+            
+            categoryCollectionView.reloadData()
+            moviesCollectionView.reloadData()
         }
     }
 }
